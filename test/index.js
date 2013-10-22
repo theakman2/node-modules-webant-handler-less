c69b3cd915802b8667310f998cb2231bc2af6f61
A Kazim <theakman2@hotmail.com>
2013-10-22 07:43:19 +0100

    Initial commit.

---
+++ test/index.js
@@ -0,0 +1,98 @@
+var Handler = require("../lib/index.js");
+var HandlerBase = require("./lib/Handler.js");
+
+function createHandler(Handler,settings) {
+	var handlerBase = new HandlerBase(settings);
+	
+	Handler.prototype = handlerBase;
+	Handler.prototype.constructor = Handler;
+	
+	return new Handler();
+}
+
+var lessApi = require("less");
+
+var tests = {
+	"test less 1":function(assert,done) {
+		var handler = createHandler(Handler);
+		var data = {
+			filePath:"https://sfi9s.sdf.sd/vk93k.handlebars?bla=3",
+			raw:"https://sfi9s.sdf.sd/vk93k.handlebars?bla=3",
+			requireType:"comment"
+		};
+		handler.willHandle(data,function(err,yes){
+			assert.strictEqual(err,null,"Handler should not report any errors.");
+			assert.strictEqual(yes,false,"Handler should not claim to handle this file.");
+			done();
+		});
+	},
+	"test less 2":function(assert,done) {
+		var handler = createHandler(Handler);
+		var data = {
+			filePath:__dirname+"/path/to/bad/file.css",
+			raw:"file.css",
+			requireType:"function"
+		};
+		handler.willHandle(data,function(err,yes){
+			assert.strictEqual(err,null,"Handler should not report any errors.");
+			assert.strictEqual(yes,false,"Handler should not claim to handle this file.");
+			done();
+		});
+	},
+	"test less correct file type":function(assert,done) {		
+		var handler = createHandler(Handler);
+		var data = {
+			filePath:__dirname+"/styles.less",
+			requireType:"comment"
+		};
+		handler.handle(data,function(resp){
+			assert.deepEqual(
+				resp,
+				{
+					type:"internalCss",
+					content:".foo .bar {\n  color: #eeccdd;\n}"
+				},
+				"Handler should call 'update' correctly."
+			);
+			done();
+		});
+	},
+	"test less correct file type with compression (2)":function(assert,done) {
+		var handler = createHandler(Handler,{compress:true});
+		var data = {
+			filePath:__dirname+"/styles.less",
+			requireType:"comment"
+		};
+		handler.handle(data,function(resp){
+			assert.deepEqual(
+				resp,
+				{
+					type:"internalCss",
+					content:".foo .bar{color:#ecd}"
+				},
+				"Handler should call 'update' correctly."
+			);
+			done();
+		});
+	},
+	"test less correct file type with compression and as javascript":function(assert,done) {		
+		var handler = createHandler(Handler,{compress:true});
+		var data = {
+			filePath:__dirname+"/styles.less",
+			requireType:"function"
+		};
+		handler.handle(data,function(resp){
+			assert.deepEqual(
+				resp,
+				{
+					type:"internalJs",
+					content:'require("@@css/addStylesheet")(".foo .bar{color:#ecd}");'
+				},
+				"Handler should call 'update' correctly."
+			);
+			done();
+		});
+	}
+};
+
+require("test").run(tests);
